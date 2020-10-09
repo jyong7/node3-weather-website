28792bded4c71a8166354ab3e1d57e066be9c88b
James <jyong1902@gmail.com>
2020-10-09 13:11:59 +0200

    texto agregado en about

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.get('',(req, res)=>{
 
 app.get('/about', (req,res)=>{
     res.render('about',{
-        title: "Mi pana miguel",
+        title: "Sobre nosotros",
         name: "James Yong"
     })
 })
