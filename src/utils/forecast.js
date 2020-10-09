604646dc80a9d38696f931e1c79124e87f979b10
James <jyong1902@gmail.com>
2020-10-09 13:26:41 +0200

    Cambios en la informacion del clima

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -6,11 +6,11 @@ const forecast=(latitude,altitude,callback)=>{
     request({url, json:true},(error, {body})=>{
         //console.log(response.body.current)
         if(error){
-            callback('No se peude conectar con el servicio del clima',undefined)
+            callback('No se ha podido conectar con el servicio del clima',undefined)
         }else if(body.error){
             callback('No se puede encontrar la ubicacion',undefined)        
         }else{
-            callback(undefined,body.current.weather_descriptions[0]+'- la temperatura es: '+body.current.temperature + ', pero se siente como si hiciera: ' + body.current.feelslike)
+            callback(undefined,'Ultima actualizaci&#243;n'+body.current.observation_time+'Descripcion del clima: '+body.current.weather_descriptions[0]+'\n La temperatura es: '+body.current.temperature + ', pero se siente como si hiciera: ' + body.current.feelslike+'\n Probabilidad de lluvia: '+ body.current.precip+'\n Humedad: '+body.current.humidity)
     }})
 }
 
